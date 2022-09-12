2a43a874d91195cac05e5900d0b4d8a66cacc70f
prodecmd <58593399+prodecmd@users.noreply.github.com>
2022-09-12 16:24:02 +0100

    Added script domains-to-ips.ps1, added test directory with test data.

---
+++ scripts/domains-to-ips.ps1
@@ -0,0 +1,11 @@
+﻿# Creates list of IPs in txt file when fed list of domains in txt file.
+# Usage: Create txt file called domains.txt with a single domain on each line. Run script in directory containing domains.txt outputs ips.txt
+
+$ips = @()
+
+foreach ($domain in [System.IO.File]::ReadLines("$(PWD)\domains.txt")) {
+    $tmpips = [System.Net.Dns]::GetHostEntry($domain).AddressList.IPAddressToString
+    $ips += $tmpips
+    }
+
+Out-File -FilePath .\ips.txt -InputObject $ips -Encoding utf8
